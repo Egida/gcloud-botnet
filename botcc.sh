6c1e25f342b9bb4db684f407aafe62c9bdb687ec
Matvey <mathway.home@gmail.com>
2023-03-04 02:32:09 +0300

    add --threads option in the help message

--- botcc.sh
+++ botcc.sh
@@ -15,6 +15,7 @@ usage()
 	printf "$fmt" "--attack <Layer:attack>" "Run this MHDDoS attack on bots"
 	printf "$fmt" "--victim <victim_spec>" "Victim specification, like in MHDDoS."
 	printf "$fmt" "--time <seconds>" "Attack time frame length in seconds."
+	printf "$fmt" "-T, --threads" "Count of attack threads on each bot"
 	printf "$fmt" "-l, --list" "List bots in selected botnet."
 	printf "$fmt" "--async" "Launch things asynchronously."
 	printf "$fmt" "-t,--no-tty" "Don't allocate pty device"
